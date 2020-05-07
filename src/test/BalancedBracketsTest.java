6061322fa351cfe8085cc92d3a44ea8985d49096
Marms <maryarmstrong314@gmail.com>
2020-05-07 18:13:15 -0500

    Added 12 tests and corrected issue where out of order brackets (seen as ][) would still read as balanced.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,70 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
 
+    @Test
+    public void hasBracketsAroundWords() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void hasBracketsSomewhereInTheMiddle() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void hasBracketsAtTheBeginning() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void hasBracketsAtTheEnd() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode[]"));
+    }
+
+    @Test
+    public void hasNoBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void hasMissingRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[LaunchCode"));
+    }
+
+    @Test
+    public void hasMissingLeftBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]"));
+    }
+
+    @Test
+    public void hasOnlyBackwardsBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void hasBackwardsBracketsBetweenWords() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void hasExtraLeftBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[Launch]Code"));
+    }
+
+    @Test
+    public void hasExtraRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launch]Code]"));
+    }
 
 }
